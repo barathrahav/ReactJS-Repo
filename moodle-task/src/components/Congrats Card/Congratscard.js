7754c4c8ea8c7113d44d27ddccc3e98c62e1b3b3
barathrahav <barathrahav@gmail.com>
2023-07-08 17:08:44 +0530

    files added.

--- moodle-task/src/components/Congrats Card/Congratscard.js
+++ moodle-task/src/components/Congrats Card/Congratscard.js
@@ -5,18 +5,18 @@ function Congrats() {
   return (
     <div className="congratsContainer text-center p-5">
       <h1>Congratulations</h1>
-      <div className="make container mt-5 border rounded-5">
+      <div className="make m-auto mt-5 rounded-5">
         <img
           src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
           alt="Profile"
           className="imgprofile"
         />
         <div className="fw-bold fs-2">Kiran V</div>
-        <p className="congrats">Vishnu Institute of Computer Education and Technology, Bhimavaram</p>
+        <div className="congrats m-2">Vishnu Institute of Computer Education and Technology, Bhimavaram</div>
         <img
           src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
           alt="Watch"
-          className="imgwatch"
+          className="imgwatch mb-2"
         />
       </div>
     </div>
