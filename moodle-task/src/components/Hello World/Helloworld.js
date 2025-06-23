8a66ab2021a9f6daa7750c4f590ae9b3ec8b6f46
barathrahav <barathrahav@gmail.com>
2025-06-23 07:40:44 +0530

    Updated

--- moodle-task/src/components/Hello World/Helloworld.js
+++ moodle-task/src/components/Hello World/Helloworld.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-export function Hello(){
-    return(
-        <h1 className="position-absolute top-50 start-50 translate-middle">Hello World !</h1>
-    );
+export function Hello() {
+  return (
+    <h1 className="position-absolute top-50 start-50 translate-middle">
+      Hello World !
+    </h1>
+  );
 }
